cdc20b6e525d3ef34a89a2787a2b380eb8962b9b
NoobiesDoobies <carlisoeryan20@gmail.com>
2024-12-11 12:41:31 +0700

    add SMC data v2

--- 3d_anim.m
+++ 3d_anim.m
@@ -112,13 +112,13 @@ drawnow
 %load("PID_Massa10kg.mat");
 
 % SMC
-%load("SMC_Massa1koma5kg.mat");
-%load("SMC_Massa5kg.mat");
-%load("SMC_Massa10kg.mat");
+%load("SMC_Massa1koma5kg_v2.mat");
+load("SMC_Massa5kg_v2.mat");
+%load("SMC_Massa10kg_v2.mat");
 
 
 % MPC
-load("trajectory_data_1koma5kg.mat");
+%load("trajectory_data_1koma5kg.mat");
 %load("trajectory_data_5kg.mat");
 %load("trajectory_data_10kg.mat");
 longitude = y;
